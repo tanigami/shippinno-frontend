955185a082edd476f180acc07e0827038f8cfd47
Hirofumi Tanigami <tanigami@gmail.com>
2017-11-28 18:53:21 +0900

    Introduce element ui

--- src/components/ShipmentList.vue
+++ src/components/ShipmentList.vue
@@ -1,18 +1,18 @@
 <template>
-  <table>
-    <tr>
-      <th>ID</th>
-      <th>Order ID</th>
-      <th>Order number</th>
-      <th>Warehousing</th>
-    </tr>
-    <tr v-for="shipment in shipments">
-      <td>{{ shipment.id }}</td>
-      <td>{{ shipment.order.id }}</td>
-      <td>{{ shipment.order.order_number }}</td>
-      <td>{{ shipment.name }}</td>
-    </tr>
-  </table>
+  <el-table :data="shipments" style="width: 100%">
+    <el-table-column type="expand">
+      <template slot-scope="props">
+        <el-table :data="props.row.shipment_lines" style="width: 100%">
+          <el-table-column label="Item Name" prop="item_name"/>
+          <el-table-column label="Unit Price" prop="unit_price"/>
+          <el-table-column label="Quantity" prop="quantity"/>
+        </el-table>
+      </template>
+    </el-table-column>
+    <el-table-column label="ID" prop="id"/>
+    <el-table-column label="Order Number" prop="order.order_number"/>
+    <el-table-column label="Warehousing" prop="warehousing.name"/>
+  </el-table>
 </template>
 
 <script>
